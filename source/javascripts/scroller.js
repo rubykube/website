48953b016358d083a76e52839516b7d5156ab9b6
alexander <sashaersh27@gmail.com>
2018-08-18 11:29:41 +0300

    Blog nav menu item added

--- source/javascripts/scroller.js
+++ source/javascripts/scroller.js
@@ -6,11 +6,64 @@ $(document).ready(function () {
   let currentSection = 0;
   
   if($(window).width() > 992){
-    $(document.body).on('DOMMouseScroll mousewheel', function (e) {
-        if (e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) {
-          pathScroll = 'down';
-        } else {
-          pathScroll = 'up';
+    $('.js-scrollTo').on('click', function(e) {
+      e.preventDefault()
+
+      if (location.pathname.substr(1) === "blog"){
+        let page = $(this).attr('href');
+        location.href = '../' + page;
+      } else {
+        let page = $(this).attr('href')
+        $('html, body').animate({
+          scrollTop: page ? $(page).offset().top : 0
+        }, 500)
+
+        if (page) {
+          window.location.hash = page
+        }
+      }
+    })
+
+  $('section').each(function (i) {
+    sections.push({
+      id: $(this).attr('id'),
+      offset: $(this).offset().top
+    })
+
+      let page = $(this).attr('href')
+      $('html, body').animate({
+        scrollTop: page ? $(page).offset().top : 0
+      }, 500)
+
+      if (page) {
+        window.location.hash = page
+      }
+    })
+
+    $('section').each(function (i) {
+      sections.push({
+        id: $(this).attr('id'),
+        offset: $(this).offset().top
+      })
+
+      if ($(this).attr('id') === window.location.hash) {
+        currentSection = i
+        setNavColor()
+      }
+    })
+
+    $(window).on('wheel', function (event) {
+      event.preventDefault()
+
+      if (isScrolling) {
+        clearTimeout(scrollTimeout)
+      } else {
+        isScrolling = true
+
+        if (event.originalEvent.deltaY < 0) {
+          prevSection()
+        } else if (event.originalEvent.deltaY > 0) {
+          nextSection()
         }
 
         currentSection = -1;
