023d962eaf4d1445f0dea45d38a54a59ee4c79a9
akushniruk <andrewfouren677@gmail.com>
2018-08-22 00:32:06 +0300

    add gallery & fix grid features

--- source/javascripts/slideimg.js
+++ source/javascripts/slideimg.js
@@ -1,26 +1,20 @@
-$(document).ready(function(){
+$('#recipeCarousel').carousel({
+  interval: 10000
+})
 
-  $('#carouselSlide').carousel({ 
-    interval: 2000
-  });
-
-  $('#carouselSlide').on('slide.bs.carousel', function (e) {
-    let $e = $(e.relatedTarget);
-    let idx = $e.index();
-    let itemsPerSlide = 4;
-    let totalItems = $('.carousel-item').length;
+$('.carousel .carousel-item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
     
-    if (idx >= totalItems-(itemsPerSlide-1)) {
-        var it = itemsPerSlide - (totalItems - idx);
-        for (var i=0; i<it; i++) {
-            if (e.direction=="left") {
-                $('.carousel-item').eq(i).appendTo('.carousel-inner');
-            }
-            else {
-                $('.carousel-item').eq(0).appendTo('.carousel-inner');
-            }
-        }
+    for (var i=0;i<2;i++) {
+        next=next.next();
+        if (!next.length) {
+        	next = $(this).siblings(':first');
+      	}
+        
+        next.children(':first-child').clone().appendTo($(this));
       }
-  });
-  
 });
