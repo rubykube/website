2941105ec5f4e1e67f485d2152233a06b3c45656
fouren <andrewfouren677@gmail.com>
2018-08-22 13:57:29 +0300

    fix nav & scroll

--- source/javascripts/scroller.js
+++ source/javascripts/scroller.js
@@ -6,11 +6,13 @@ let sections = []
 let currentSection = 0
 
 $(document).ready(function() {
-  if($(window).width() > 992){
     $('.js-scrollTo').on('click', function(e) {
       e.preventDefault()
 
-      if (location.pathname.substr(1) === "blog"){
+      if (location.pathname.match(/blog\//) == "blog/"){
+        let page = $(this).attr('href');
+        location.href = '../../' + page;
+      } else if (location.pathname.match(/blog/) == "blog"){
         let page = $(this).attr('href');
         location.href = '../' + page;
       } else {
@@ -25,62 +27,61 @@ $(document).ready(function() {
       }
     })
 
-  $('section').each(function (i) {
-    sections.push({
-      id: $(this).attr('id'),
-      offset: $(this).offset().top
-    })
-
-    if (location.pathname.substr(1) === "blog"){
-      let page = $(this).attr('href');
-      window.location.href = '/' + page;
-    } else {
-      let page = $(this).attr('href')
-      $('html, body').animate({
-        scrollTop: page ? $(page).offset().top : 0
-      }, 500)
-
-      if (page) {
-        window.location.hash = page
-      }
-    }
-  })
-
     $('section').each(function (i) {
       sections.push({
         id: $(this).attr('id'),
         offset: $(this).offset().top
       })
 
-      if ($(this).attr('id') === window.location.hash) {
-        currentSection = i
-        setNavColor()
+      if (location.pathname.substr(1) === "blog"){
+        let page = $(this).attr('href');
+        window.location.href = '/' + page;
+      } else {
+        let page = $(this).attr('href')
+        $('html, body').animate({
+          scrollTop: page ? $(page).offset().top : 0
+        }, 500)
+
+        if (page) {
+          window.location.hash = page
+        }
       }
     })
 
-    $(window).on('wheel', function (event) {
-      event.preventDefault()
+    if(location.pathname.match(/blog/) != "blog"){
+      $('section').each(function (i) {
+        sections.push({
+          id: $(this).attr('id'),
+          offset: $(this).offset().top
+        })
 
-      if (isScrolling) {
-        clearTimeout(scrollTimeout)
-      } else {
-        isScrolling = true
+        if ($(this).attr('id') === window.location.hash) {
+          currentSection = i
+          setNavColor()
+        }
+      })
 
-        if (event.originalEvent.deltaY < 0) {
-          prevSection()
-        } else if (event.originalEvent.deltaY > 0) {
-          nextSection()
+      $(window).on('wheel', function (event) {
+        event.preventDefault()
+
+        if (isScrolling) {
+          clearTimeout(scrollTimeout)
+        } else {
+          isScrolling = true
+
+          if (event.originalEvent.deltaY < 0) {
+            prevSection()
+          } else if (event.originalEvent.deltaY > 0) {
+            nextSection()
+          }
+
+          $('html, body').animate({ scrollTop: sections[currentSection].offset }, 500)
         }
 
-        $('html, body').animate({ scrollTop: sections[currentSection].offset }, 500)
-        window.location.hash = `#${sections[currentSection].id}`
+        scrollTimeout = setTimeout(function () { isScrolling = false }, 100)
+      })
+    }
 
-        setNavColor()
-      }
-
-      scrollTimeout = setTimeout(function () { isScrolling = false }, 100)
-    })
-  } else if ($(window).width() <= 992) {
     $(window).scroll(function(){
       let currentclass
       $('section').each(function(){
@@ -95,10 +96,9 @@ $(document).ready(function() {
         $('#sideNav').removeClass('red')
       }
 
-      $('#sideNav').removeClass('about components features team followus')
+      $('#sideNav').removeClass('about components features gallery technology releases tutorials blog team followus')
       $('#sideNav').addClass(currentclass)
     })
-  }
 })
 
 function prevSection() {
