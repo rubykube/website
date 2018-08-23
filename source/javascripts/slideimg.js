8f0ec5a6980e2d113d7d04f78c23cec2eb6b6f86
fouren <andrewfouren677@gmail.com>
2018-08-23 16:40:34 +0300

    mobile slide features

--- source/javascripts/slideimg.js
+++ source/javascripts/slideimg.js
@@ -1,22 +1,37 @@
 $(document).ready(function(){
-  $('#recipeCarousel').carousel({
-    interval: 1500
-  })
+  $('#carouselSlide').carousel({ 
+    interval: 2000
+  });
 
-  $('.carousel .carousel-item').each(function(){
-      var next = $(this).next();
-      if (!next.length) {
-      next = $(this).siblings(':first');
-      }
-      next.children(':first-child').clone().appendTo($(this));
-      
-      for (var i=0;i<2;i++) {
-          next=next.next();
-          if (!next.length) {
-            next = $(this).siblings(':first');
-          }
-          
-          next.children(':first-child').clone().appendTo($(this));
+  $('#carouselSlide').on('slide.bs.carousel', function (e) {
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 4;
+    var totalItems = $('.carousel-item').length;
+    
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
         }
+    }
+});
+
+  $(document).ready(function() {
+    $('a.thumb').click(function(event){
+      event.preventDefault();
+      var content = $('.modal-body');
+      content.empty();
+        var title = $(this).attr("title");
+        $('.modal-title').html(title);        
+        content.html($(this).html());
+        $(".modal-profile").modal({show:true});
+    });
+
   });
 });
