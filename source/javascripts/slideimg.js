f33e2afb6ad16add3d18cef9258a24189fa42f49
fouren <andrewfouren677@gmail.com>
2018-08-23 17:49:25 +0300

    fix scroller.js

--- source/javascripts/slideimg.js
+++ source/javascripts/slideimg.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
+
   $('#carouselSlide').carousel({ 
     interval: 2000
   });
 
   $('#carouselSlide').on('slide.bs.carousel', function (e) {
-    var $e = $(e.relatedTarget);
-    var idx = $e.index();
-    var itemsPerSlide = 4;
-    var totalItems = $('.carousel-item').length;
+    let $e = $(e.relatedTarget);
+    let idx = $e.index();
+    let itemsPerSlide = 4;
+    let totalItems = $('.carousel-item').length;
     
     if (idx >= totalItems-(itemsPerSlide-1)) {
         var it = itemsPerSlide - (totalItems - idx);
@@ -19,19 +20,7 @@ $(document).ready(function(){
                 $('.carousel-item').eq(0).appendTo('.carousel-inner');
             }
         }
-    }
-});
-
-  $(document).ready(function() {
-    $('a.thumb').click(function(event){
-      event.preventDefault();
-      var content = $('.modal-body');
-      content.empty();
-        var title = $(this).attr("title");
-        $('.modal-title').html(title);        
-        content.html($(this).html());
-        $(".modal-profile").modal({show:true});
-    });
-
+      }
   });
+  
 });
