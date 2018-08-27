4b7518d7c817c2e52649bbebc7da8621b07087c1
fouren <andrewfouren677@gmail.com>
2018-08-27 11:24:30 +0300

    fix scrooler

--- source/javascripts/scroller.js
+++ source/javascripts/scroller.js
@@ -1,58 +1,35 @@
 import $ from 'jquery'
 
-let scrollTimeout
-let isScrolling = false
-let sections = []
-let currentSection = 0
+$(document).ready(function () {
+  let section = $('.section-scroll');
+  let pathScroll = 'up'; 
+  let currentSection = 0;
+  $(document.body).on('DOMMouseScroll mousewheel', function (e) {
+      if (e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) {
+        pathScroll = 'down';
+      } else {
+        pathScroll = 'up';
+      }
 
-$(document).ready(function() {
-  if(location.pathname.match(/blog/) != "blog"){
-    $('section').each(function (i) {
-      sections.push({
-        id: $(this).attr('id'),
-        offset: $(this).offset().top
-      })
-      
-        let page = $(this).attr('href')
-        $('html, body').animate({
-          scrollTop: page ? $(page).offset().top : 0
-        }, 500)
-
-        if (page) {
-          window.location.hash = page
-        }
-    })
-
-      $(window).on('wheel', function (event) {
-        event.preventDefault()
-
-        if (isScrolling) {
-          clearTimeout(scrollTimeout)
-        } else {
-          isScrolling = true
-
-          if (event.originalEvent.deltaY < 0) {
-            prevSection()
-          } else if (event.originalEvent.deltaY > 0) {
-            nextSection()
+      currentSection = -1;
+      section.each(function(i){
+          if (currentSection<0 && ($(this).offset().top >= $(window).scrollTop())) {
+            currentSection = i;
           }
+      });
+      if (pathScroll == 'up' && currentSection > 0) {
+        currentSection--;
+      }
+      if (pathScroll == 'down' && currentSection < section.length) {
+        currentSection++;
+      }
 
-          $('html, body').animate({ scrollTop: sections[currentSection].offset }, 500)
-        }
-
-        scrollTimeout = setTimeout(function () { isScrolling = false }, 100)
-      })
-  }
-})
-
-function prevSection() {
-  if (currentSection > 0) {
-    currentSection -= 1
-  }
-}
-
-function nextSection() {
-  if (currentSection < sections.length - 1) {
-    currentSection += 1
-  }
-}
+      $('html,body').stop().animate({
+          scrollTop: section.eq(currentSection).offset().top
+      }, 600);
+      return false;
+  });
+  $(window).resize(function () {
+      $('html,body').scrollTop(section.eq(currentSection).offset().top);
+  });
+});
